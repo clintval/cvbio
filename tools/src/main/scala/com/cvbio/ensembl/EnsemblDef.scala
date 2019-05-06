367376305797f8c2b7cada9b168c2543953f8ef3
Clint Valentine <valentine.clint@gmail.com>
2019-05-06 09:42:03 -0700

    Match any amount of whitespace in a species name for underscore replacement (#3)

---
+++ tools/src/main/scala/com/cvbio/ensembl/EnsemblDef.scala
@@ -0,0 +1,8 @@
+package com.cvbio.ensembl
+
+/** Constants across all Ensembl-related tools. */
+object EnsemblDef {
+
+  /** The FTP hostname for Ensembl web resources. */
+  val FtpHost: String = "ftp.ensembl.org"
+}
