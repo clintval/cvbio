0189c9f5109f1a5767db612522cad0e3becfcaec
Clint Valentine <valentine.clint@gmail.com>
2019-10-31 11:38:08 -0700

    Squelched scaladoc warnings

--- commons/src/io/cvbio/commons/collection/BufferedLineWriter.scala
+++ commons/src/io/cvbio/commons/collection/BufferedLineWriter.scala
@@ -3,7 +3,7 @@ package io.cvbio.commons.collection
 import io.cvbio.commons.CommonsDef._
 import java.io.BufferedWriter
 
-/** Enable a [[BufferedWriter]] to write a complete line. */
+/** Enable a [[java.io.BufferedWriter]] to write a complete line. */
 trait BufferedLineWriter extends BufferedWriter {
 
   /** Write a string and then suffix with a newline separator to form a proper line. */
