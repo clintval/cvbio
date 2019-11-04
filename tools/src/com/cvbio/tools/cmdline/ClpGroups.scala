d634142b4a5bd965d512b4570530614c838fb62f
Clint Valentine <valentine.clint@gmail.com>
2019-11-03 23:43:21 -0800

    Re-order ClpGroups

--- tools/src/com/cvbio/tools/cmdline/ClpGroups.scala
+++ tools/src/com/cvbio/tools/cmdline/ClpGroups.scala
@@ -5,14 +5,14 @@ import com.fulcrumgenomics.sopt.cmdline.ClpGroup
 /** The command line groups. */
 object ClpGroups {
 
-  class _Util extends ClpGroup {
-    override val name: String = "Utilities"
-    override val description: String = "Utility programs."
-  }
-
   class _SamOrBam extends ClpGroup {
     override val name: String = "SAM/BAM"
     override val description: String = "Tools for manipulating SAM, BAM, and related data."
+  }
+
+  class _Util extends ClpGroup {
+    override val name: String = "Utilities"
+    override val description: String = "Utility programs."
   }
 
   final val SamOrBam = classOf[_SamOrBam]
