07c2e334230b72f56ad0aba89f150e90ae2b2353
Clint Valentine <valentine.clint@gmail.com>
2019-12-10 12:59:24 -0500

    Breaking: Rename RelabelReferenceNames to UpdateDataContigNames (#65)

--- tools/test/src/io/cvbio/tools/util/UpdateDataContigNamesTest.scala
+++ tools/test/src/io/cvbio/tools/util/UpdateDataContigNamesTest.scala
@@ -6,7 +6,7 @@ import com.fulcrumgenomics.commons.util.CaptureSystemStreams
 import io.cvbio.commons.effectful.Io
 import io.cvbio.testing.UnitSpec
 
-class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
+class UpdateDataContigNamesTest extends UnitSpec with CaptureSystemStreams {
 
   /** Default delimiter for tests. */
   val Delimiter: Char = '\t'
@@ -24,34 +24,34 @@ class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
     file
   }
 
-  "RelabelReferenceNames.relabel" should "successfully relabel a valid column position" in {
+  "UpdateDataContigNames.update" should "successfully update a valid column position" in {
     val line     = Seq("chr1", "200030", "400000", "interval-name").mkString(Delimiter.toString)
     val expected = Seq("1", "200030", "400000", "interval-name").mkString(Delimiter.toString)
-    val actual   = RelabelReferenceNames.relabel(line, Seq(0), Delimiter, Hg38UcscToEnsemblAutosomes)
+    val actual   = UpdateDataContigNames.update(line, Seq(0), Delimiter, Hg38UcscToEnsemblAutosomes)
     actual shouldBe expected
   }
 
-  it should "successfully relabel multiple valid column positions" in {
+  it should "successfully update multiple valid column positions" in {
     val line     = Seq("chr1", "chr2", "chr3", "chr4").mkString(Delimiter.toString)
     val expected = Seq("1", "2", "3", "chr4").mkString(Delimiter.toString)
-    val actual   = RelabelReferenceNames.relabel(line, Seq(0, 1, 2), Delimiter, Hg38UcscToEnsemblAutosomes)
+    val actual   = UpdateDataContigNames.update(line, Seq(0, 1, 2), Delimiter, Hg38UcscToEnsemblAutosomes)
     actual shouldBe expected
   }
 
   it should "raise an exception by default if a lookup function fails" in {
     val line = Seq("chr29", "200030", "400000", "interval-name").mkString(Delimiter.toString)
-    an[NoSuchElementException] shouldBe thrownBy { RelabelReferenceNames.relabel(line, Seq(0), Delimiter, Hg38UcscToEnsemblAutosomes) }
+    an[NoSuchElementException] shouldBe thrownBy { UpdateDataContigNames.update(line, Seq(0), Delimiter, Hg38UcscToEnsemblAutosomes) }
   }
 
-  "RelabelReferenceNames.buildMapping" should "read a mapping from an iterator of lines" in {
-    RelabelReferenceNames.buildMapping(MappingLines.iterator, Delimiter) shouldBe Hg38UcscToEnsemblAutosomes
+  "UpdateDataContigNames.buildMapping" should "read a mapping from an iterator of lines" in {
+    UpdateDataContigNames.buildMapping(MappingLines.iterator, Delimiter) shouldBe Hg38UcscToEnsemblAutosomes
   }
 
   it should "read a mapping from two-column delimited file" in {
-    RelabelReferenceNames.buildMapping(MappingFile, Delimiter) shouldBe Hg38UcscToEnsemblAutosomes
+    UpdateDataContigNames.buildMapping(MappingFile, Delimiter) shouldBe Hg38UcscToEnsemblAutosomes
   }
 
-  "RelabelReferenceNames" should "relabel a text file" in {
+  "UpdateDataContigNames" should "update a text file" in {
     val original = Seq(Seq("chr1", "2", "3"), Seq("chr4", "4", "5"))
     val expected = Seq(Seq("1", "2", "3"), Seq("4", "4", "5"))
 
@@ -59,25 +59,25 @@ class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
     val outfile  = tempFile()
     Io.writeLines(infile, original.map(_.mkString(Delimiter.toString)))
 
-    val relabel = new RelabelReferenceNames(infile, outfile, MappingFile, columns = Seq(0), delimiter = Delimiter)
-    relabel.execute()
+    val update = new UpdateDataContigNames(infile, outfile, mapping = MappingFile, columns = Seq(0), delimiter = Delimiter)
+    captureLogger { () => update.execute() }
 
     val actual = Io.readLines(outfile).map(_.split(Delimiter)).toList
     actual should contain theSameElementsInOrderAs expected
   }
 
-  it should "raise an exception by default if a lookup function fails and drop is false" in {
+  it should "raise an exception by default if a lookup function fails and skip missing is false" in {
     val original = Seq(Seq("chr1", "2", "3"), Seq("chr4", "4", "5"), Seq("illegal", "5", "6"))
 
     val infile   = tempFile()
     val outfile  = tempFile()
     Io.writeLines(infile, original.map(_.mkString(Delimiter.toString)))
 
-    val relabel = new RelabelReferenceNames(infile, outfile, MappingFile, columns = Seq(0), delimiter = Delimiter, drop = false)
-    a[NoSuchElementException] shouldBe thrownBy { relabel.execute() }
+    val update = new UpdateDataContigNames(infile, outfile, mapping = MappingFile, columns = Seq(0), delimiter = Delimiter, skipMissing = false)
+    a[NoSuchElementException] shouldBe thrownBy { update.execute() }
   }
 
-  it should "drop any records with items not in the mapping when drop is true" in {
+  it should "skip any records with items not in the mapping when skip missing is true" in {
     val original = Seq(Seq("chr1", "2", "3"), Seq("chr4", "4", "5"), Seq("illegal", "5", "6"))
     val expected = Seq(Seq("1", "2", "3"), Seq("4", "4", "5"))
 
@@ -85,14 +85,14 @@ class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
     val outfile  = tempFile()
     Io.writeLines(infile, original.map(_.mkString(Delimiter.toString)))
 
-    val relabel = new RelabelReferenceNames(infile, outfile, MappingFile, columns = Seq(0), delimiter = Delimiter, drop = true)
-    noException shouldBe thrownBy { captureLogger { () => relabel.execute() } }
+    val update = new UpdateDataContigNames(infile, outfile, mapping = MappingFile, columns = Seq(0), delimiter = Delimiter, skipMissing = true)
+    noException shouldBe thrownBy { captureLogger { () => update.execute() } }
 
     val actual = Io.readLines(outfile).map(_.split(Delimiter)).toList
     actual should contain theSameElementsInOrderAs expected
   }
 
-  it should "drop any records with items not in the mapping when drop is true, but not lines starting with a skip prefix" in {
+  it should "skip any records with items not in the mapping when skip missing is true, but not lines starting with a skip prefix" in {
     val original = Seq(Seq("chr1", "2", "3"), Seq("# comment"), Seq("chr4", "4", "5"), Seq("illegal", "5", "6"))
     val expected = Seq(Seq("1", "2", "3"), Seq("# comment"), Seq("4", "4", "5"))
 
@@ -100,14 +100,14 @@ class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
     val outfile  = tempFile()
     Io.writeLines(infile, original.map(_.mkString(Delimiter.toString)))
 
-    val relabel = new RelabelReferenceNames(infile, outfile, MappingFile, columns = Seq(0), delimiter = Delimiter, drop = true)
-    noException shouldBe thrownBy { captureLogger { () => relabel.execute() } }
+    val update = new UpdateDataContigNames(infile, outfile, mapping = MappingFile, columns = Seq(0), delimiter = Delimiter, skipMissing = true)
+    noException shouldBe thrownBy { captureLogger { () => update.execute() } }
 
     val actual = Io.readLines(outfile).map(_.split(Delimiter)).toList
     actual should contain theSameElementsInOrderAs expected
   }
 
-  it should "write-out records with skip characters directly to the output file" in {
+  it should "write-out records with comment characters as prefixes, directly to the output file" in {
     val skipPrefixes = Seq("#", "track")
 
     val original = Seq(Seq("chr1", "2", "3"), Seq("# this is a comment"), Seq("track: this is a track"), Seq("chr4", "4", "5"))
@@ -117,8 +117,8 @@ class RelabelReferenceNamesTest extends UnitSpec with CaptureSystemStreams {
     val outfile  = tempFile()
     Io.writeLines(infile, original.map(_.mkString(Delimiter.toString)))
 
-    val relabel = new RelabelReferenceNames(infile, outfile, MappingFile, columns = Seq(0), delimiter = Delimiter, skipPrefixes = skipPrefixes)
-    relabel.execute()
+    val update = new UpdateDataContigNames(infile, outfile, mapping = MappingFile, columns = Seq(0), delimiter = Delimiter, commentChars = skipPrefixes)
+    captureLogger { () => update.execute() }
 
     val actual = Io.readLines(outfile).map(_.split(Delimiter)).toList
     actual should contain theSameElementsInOrderAs expected
