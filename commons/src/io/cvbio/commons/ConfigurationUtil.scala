89948fbbb6939ffe9b8d0c87225a38d76831e25d
Clint Valentine <valentine.clint@gmail.com>
2019-12-22 02:25:34 -0500

    Add a root abstraction

--- commons/src/io/cvbio/commons/ConfigurationUtil.scala
+++ commons/src/io/cvbio/commons/ConfigurationUtil.scala
@@ -1,11 +1,13 @@
 package io.cvbio.commons
 
 import java.io.File
-import java.nio.file.{FileVisitOption, Files, Path}
+import java.nio.file.{FileVisitOption, Files, Path, Paths}
 
+import com.fulcrumgenomics.commons.io.PathUtil
 import com.fulcrumgenomics.commons.io.PathUtil.{basename, extensionOf, pathTo}
 import io.cvbio.commons.CommonsDef._
 import com.fulcrumgenomics.commons.io.PathUtil.pathTo
+
 import scala.util.Properties.isMac
 
 /** Helper methods for configuration. */
@@ -14,11 +16,14 @@ object ConfigurationUtil {
   /** The system PATH variable. */
   lazy val Path: String = sys.env("PATH")
 
+  /** The root of this Unix system. */
+  val Root: Path = PathUtil.pathTo("/")
+
   /** The paths defined within the PATH variable. */
   lazy val SystemPaths: Seq[FilePath] = Path.split(File.pathSeparator).map(pathTo(_))
 
   /** The location on Mac operating systems where applications are installed. */
-  val MacApplicationRoot: DirPath = pathTo("/Applications")
+  val MacApplicationRoot: DirPath = Root.resolve("Applications")
 
   /** Searches the system path for an executable and returns the full path, if found. */
   def findExecutableInPath(executable: String): Option[Path] = {
