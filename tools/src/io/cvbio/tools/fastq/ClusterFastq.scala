cc7543333b8371c795677511de71e0a8c5599e2b
Clint Valentine <valentine.clint@gmail.com>
2019-12-22 00:39:37 -0500

    Bit o' style

--- tools/src/io/cvbio/tools/fastq/ClusterFastq.scala
+++ tools/src/io/cvbio/tools/fastq/ClusterFastq.scala
@@ -37,10 +37,11 @@ object ClusterFastq {
   /** Detect the quality encoding in a collection of FASTQ files. */
   def detectQualityEncoding(input: Seq[PathToFastq], logger: Option[Logger] = None): QualityEncoding = {
     val detector  = new QualityEncodingDetector
+    input.map(FastqSource.apply)
     detector.sample(input.iterator.flatMap(FastqSource.apply).map(_.quals))
-    val encodings = detector.rankedCompatibleEncodings (q = 30)
+    val encodings = detector.rankedCompatibleEncodings(q = 30)
     require(encodings.nonEmpty, "Could not determine quality score encoding in FASTQ. No known encodings are valid for all observed qualities.")
-    if (encodings.size > 1) logger.foreach(_.warning(s"Making ambiguous determination about fastq's quality encoding; possible encodings: ${encodings.mkString (", ")}."))
+    if (encodings.size > 1) logger.foreach(_.warning(s"Making ambiguous determination about FASTQ's quality encoding; possible encodings: ${encodings.mkString (", ")}."))
     logger.foreach(_.info(s"Auto-detected quality format as: ${encodings.head}"))
     encodings.head
   }
