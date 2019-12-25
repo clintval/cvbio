38f4bca4a9d0b2a1950ee14dd8b35496a86ca74a
Clint Valentine <valentine.clint@gmail.com>
2019-12-25 17:57:14 -0500

    Roundtrip test of PackageInfo

--- commons/test/src/io/cvbio/commons/environ/CondaTest.scala
+++ commons/test/src/io/cvbio/commons/environ/CondaTest.scala
@@ -1,7 +1,9 @@
 package io.cvbio.commons.environ
 
 import io.cvbio.commons.effectful.CommandLineTool.ToolException
+import io.cvbio.commons.environ.Conda.PackageInfo
 import io.cvbio.testing.UnitSpec
+import spray.json._
 
 import scala.util.{Failure, Success}
 
@@ -24,5 +26,11 @@ class CondaTest extends UnitSpec {
 
   it should "find multiple generic packages when conda is installed" in {
     captureLogger { () => Conda.moduleAvailable(Seq("conda", "pip")) shouldBe Conda.available }
+  }
+
+  "Conda.PackageInfoJsonProtocol" should "round trip a PackageInfo class" in {
+    import io.cvbio.commons.environ.Conda.PackageInfoJsonProtocol._
+    val info = PackageInfo("http://baseurl.com", 2, "2", "bioconda", "macosx", "pip", "x86", "0.3.3")
+    info.toJson.convertTo[PackageInfo] shouldBe info
   }
 }
